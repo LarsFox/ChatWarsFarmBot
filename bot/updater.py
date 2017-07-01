d2e55a933a9848e06efd28961d8f6b7755c52e0f
Leo <leomotovskikh@gmail.com>
2017-07-01 12:04:34 +0300

    Moved send_message and update_dialogues to updater

--- bot/updater.py
+++ bot/updater.py
@@ -3,15 +3,16 @@
 Модуль для конкретных и часто используемых обращений к клиенту
 """
 
-from bot.data import WAR, WAR_COMMANDS, REGROUP
+from bot.data import WAR, WAR_COMMANDS, REGROUP, CHATS
+from sessions import SUPERGROUP_ID
 
 
 class Updater(object):
     """ Модуль для конкретных и часто используемых обращений к клиенту """
-    def __init__(self, client, logger, chats):
+    def __init__(self, client, logger):
         self.client = client
         self.logger = logger
-        self.chats = chats
+        self.chats = {}
 
     @property
     def bot_message(self):
@@ -32,6 +33,21 @@ class Updater(object):
             return message
         return WAR.get(WAR_COMMANDS.get(message))
 
+    def update_chats(self):
+        """ Обновляет список чатов на основе 10 последних диалогов """
+        _, entities = self.client.get_dialogs(10)
+
+        for entity in entities:
+            name = CHATS.get(entity.id)
+
+            if name:
+                self.chats[name] = entity
+
+            elif entity.id == SUPERGROUP_ID:
+                self.chats['group'] = entity
+
+        return True
+
     def send_group(self, message):
         """
         Отправляет сообщение Супергруппе
@@ -47,3 +63,12 @@ class Updater(object):
         _, message = self.client.get_message(self.chats["trade_bot"])
         self.client.send_text(self.chats["penguin"], message)
         return True
+
+    def send_message(self, entity_key, message):
+        """ Отправляет сообщение с Маркдауном и без предпросмотра
+        entity_key: ключ, под которым записан адресат-entity
+        message: текст сообщения """
+        self.client.send_message(self.chats[entity_key],
+                                 message,
+                                 markdown=True,
+                                 no_web_page=True)
